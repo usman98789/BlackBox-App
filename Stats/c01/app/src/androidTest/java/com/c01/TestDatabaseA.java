e872744feacaff4ae4fcb3bbf848ac4804092cd8
chanhan2 <chanhan2@github.com>
2017-11-19 18:56:51 -0500

    Finished added all test cases for the database select methods

--- Stats/c01/app/src/androidTest/java/com/c01/TestDatabaseA.java
+++ Stats/c01/app/src/androidTest/java/com/c01/TestDatabaseA.java
@@ -1,12 +1,16 @@
 package com.c01;
 
 import android.content.Context;
+import android.content.SyncStats;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.icu.text.SymbolTable;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.RenamingDelegatingContext;
 import android.test.suitebuilder.annotation.LargeTest;
+import android.util.Log;
+import android.widget.Toast;
 
 import org.junit.Test;
 import org.junit.After;
@@ -20,6 +24,7 @@ import java.util.List;
 import Database.DatabaseDriver.DatabaseDriverA;
 import Database.DatabaseDriver.DatabaseInsertHelper;
 import Database.DatabaseDriver.DatabaseSelectHelper;
+import Database.DatabaseDriver.PasswordHelpers;
 import generics.EnumMapRoles;
 import generics.Roles;
 import user.Prof;
@@ -52,6 +57,8 @@ public class TestDatabaseA {
         sql = mydb.getWritableDatabase();
         mydb.onUpgrade(sql, 1, 2);
     }
+
+    //Test cases for insert
 
     @Test
     public void testInsertUser() throws Exception {
@@ -99,6 +106,7 @@ public class TestDatabaseA {
         assertEquals(50, mark , 0);
     }
 
+    //Test cases for select
     @Test
     public void testgetAssignmentMark() throws Exception {
         EnumMapRoles roleMap = new EnumMapRoles(context);
@@ -108,5 +116,71 @@ public class TestDatabaseA {
         assertEquals(59.4, DatabaseSelectHelper.getAssignmentMark(id, 1, context), 0);
     }
 
-    
+    @Test
+    public void testGetRoleOfStudent() throws Exception {
+        EnumMapRoles roleMap = new EnumMapRoles(context);
+
+        assertEquals("STUDENT",         mydb.getRole(roleMap.get(Roles.STUDENT).intValue()));
+    }
+
+    @Test
+    public void testGetRoleOfProf() throws Exception {
+        assertEquals("PROF",         mydb.getRole(roleMap.get(Roles.PROF).intValue()));
+    }
+
+    @Test
+    public void testGetUserRoleStudent() throws Exception {
+        int id = 0;
+        id = (int)mydb.insertNewUser("gagan", 19, "123street", roleMap.get(Roles.STUDENT), "123");
+        int expected = roleMap.get(Roles.STUDENT).intValue();
+        assertEquals(expected,         mydb.getUserRole(id));
+    }
+
+    @Test
+    public void testGetUserRoleProf() throws Exception {
+        int id = 0;
+        id = (int)mydb.insertNewUser("KC", 24, "UTSC", roleMap.get(Roles.PROF), "123");;
+        int expected = roleMap.get(Roles.PROF).intValue();
+        assertEquals(expected,         mydb.getUserRole(id));
+    }
+
+    @Test
+    public void testPassword() throws Exception {
+        int id = 0;
+        id = (int)mydb.insertNewUser("KC", 24, "UTSC", roleMap.get(Roles.PROF), "WOOOO");
+        String pass = PasswordHelpers.passwordHash("WOOOO");
+        assertEquals(pass,         mydb.getPassword(id));
+    }
+
+    @Test
+    public void testGetAllMessages() throws Exception {
+        User user = null;
+        int userId = 0;
+        int id = 0;
+        id = (int)mydb.insertNewUser("KC", 24, "UTSC", roleMap.get(Roles.PROF), "WOOOO");
+        userId = (int)mydb.insertNewUser("bob", 19, "123street", roleMap.get(Roles.STUDENT), "123");
+
+        int msgId = (int)mydb.insertMessage(userId, "hello");
+        int msgId2 = (int)mydb.insertMessage(userId, "I need help with problem set 1 question 2");
+        int msgId3 = (int)mydb.insertMessage(id, "Read the textbook.");
+
+        Cursor results = mydb.getAllMessages(userId);
+        String text = "";
+        Cursor results2 = mydb.getAllMessages(id);
+        String text2 = "";
+
+        while (results.moveToNext()) {
+            text += results.getString(results.getColumnIndex("MESSAGE")) + " ";
+        }
+
+        while (results2.moveToNext()) {
+            text2 += results2.getString(results.getColumnIndex("MESSAGE")) + " ";
+        }
+
+        results.close();
+        assertEquals("hello I need help with problem set 1 question 2 ", text);
+        assertEquals("Read the textbook. ", text2);
+
+
+    }
 }
