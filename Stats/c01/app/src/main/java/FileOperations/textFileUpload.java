7f68051c2b7551d0efadb580fc085d86649dfe98
DRAG0NREAP3R-PC\Drag0nReap3r <drag0nreap3r@gmail.com>
2017-11-25 15:36:10 -0500

    Created file that will read contents of a text file and return a string

---
+++ Stats/c01/app/src/main/java/FileOperations/textFileUpload.java
@@ -0,0 +1,26 @@
+package FileOperations;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+
+class textFileReader {
+    public String readFile(String fileName) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(fileName));
+        try {
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
+
+            while (line != null) {
+                sb.append(line);
+                sb.append("\n");
+                line = br.readLine();
+            }
+            return sb.toString();
+        } finally {
+            br.close();
+        }
+    }
+}
