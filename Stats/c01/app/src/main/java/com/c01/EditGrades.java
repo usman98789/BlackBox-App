ffc71df41a8cd317db40b0f31a1cfc181633f5c6
JulioZhao <julie.zhao@mail.utoronto.ca>
2017-11-26 19:01:37 -0500

    added javadoc and comment

--- Stats/c01/app/src/main/java/com/c01/EditGrades.java
+++ Stats/c01/app/src/main/java/com/c01/EditGrades.java
@@ -23,8 +23,16 @@ import generics.EnumMapRoles;
 import generics.Roles;
 import user.User;
 
+/**
+* The activity of editting grades.
+*/
 public class EditGrades extends AppCompatActivity {
 
+    /**
+    * Starts the activity.
+    * @param savedInstanceState The data it most recently supplied on
+    * @return No return value
+    */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,6 +42,12 @@ public class EditGrades extends AppCompatActivity {
 
     }
 
+    /**
+    * Displays marks.
+    * @param context The context containing the things being created
+    * @exception e Any exception
+    * @return No return value
+    */
     public void displayMarks(Context context){
         EnumMapRoles roleMap = new EnumMapRoles(context);
         ArrayList<String> displayer = new ArrayList<>();
@@ -57,6 +71,16 @@ public class EditGrades extends AppCompatActivity {
 
         view.setAdapter(adapter);
         view.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            
+            /**
+            * Responds when an item in AdapterView has been clicked.
+            * @param parent The AdapterView where the click happened
+            * @param view The view whin the AdapterView that was clicked
+            * @param position The position of the view in the adapter
+            * @param id The row id of the item that was clicked
+            * @exception e Any exception
+            * @return No return value
+            */
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Intent i = new Intent(view.getContext(),ProblemSetList.class);
@@ -68,6 +92,10 @@ public class EditGrades extends AppCompatActivity {
 
     }
 
+    /**
+    * Responds when user presses the back key.
+    * @return No return value
+    */
     @Override
     public void onBackPressed() {
         Intent intent = new Intent(getApplicationContext(), InstructorMenu.class);
